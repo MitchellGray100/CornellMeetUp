0a0e2c6796ba15730105cdc52852fcf274edc2ca
Jacob Kerr <jck268@cornell.edu>
2022-10-17 05:00:11 +0000

    Improved logging

--- userinfo/__init__.py
+++ userinfo/__init__.py
@@ -18,7 +18,6 @@ info: dict[str,str] - profile information
     time-zone: str - timezone of the user
     profile-picture-id: str - id of profile picture"""
 
-from typing import Mapping
 import os
 import json
 import logging
@@ -27,15 +26,18 @@ import azure.functions as func
 from azure.cosmos.aio import CosmosClient
 from azure.cosmos.exceptions import CosmosHttpResponseError
 
+
 ENDPOINT = os.environ.get("ENDPOINT") or ""
 KEY = os.environ.get("KEY") or ""
 DATABASE_NAME = os.environ.get("DATABASE_NAME") or ""
 CONTAINER_NAME = os.environ.get("CONTAINER_NAME") or ""
 PARTITION_KEY = os.environ.get("PARTITION_KEY") or ""
 
+
 client = CosmosClient(ENDPOINT, credential=KEY)
 database = client.get_database_client(DATABASE_NAME)
 container = database.get_container_client(CONTAINER_NAME)
+
 
 async def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('userinfo lambda triggered')
@@ -43,6 +45,7 @@ async def main(req: func.HttpRequest) -> func.HttpResponse:
     req_type = req.params.get('type')
 
     if req_type == 'get':
+        logging.info('    get request received')
         username = req.params.get('username')
         if username is None:
             return func.HttpResponse('Request malformed: username missing', status_code=400)
@@ -54,6 +57,7 @@ async def main(req: func.HttpRequest) -> func.HttpResponse:
             return func.HttpResponse(json.dumps(user_object), status_code=200)
 
     elif req_type == 'update':
+        logging.info('    update request received')
         username = req.params.get('username')
         if username is None:
             return func.HttpResponse('Request malformed: username missing', status_code=400)
@@ -71,6 +75,7 @@ async def main(req: func.HttpRequest) -> func.HttpResponse:
             return func.HttpResponse('Okay', status_code=200)
 
     elif req_type == 'add':
+        logging.info('    add request received')
         try:
             body: dict[str,str] = req.get_json()
             container.create_item(body)
@@ -82,6 +87,7 @@ async def main(req: func.HttpRequest) -> func.HttpResponse:
             return func.HttpResponse('Okay', status_code=200)
         
     elif req_type == 'delete':
+        logging.info('    delete request received')
         username = req.params.get('username')
         if username is None:
             return func.HttpResponse('Request malformed: username missing', status_code=400)
@@ -93,4 +99,5 @@ async def main(req: func.HttpRequest) -> func.HttpResponse:
             return func.HttpResponse('Okay', status_code=200)
     
     else:
+        logging.warn('    unknown request received')
         return func.HttpResponse('Request malformed: unknown request type', status_code=400)
