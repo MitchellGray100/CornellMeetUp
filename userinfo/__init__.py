915aa148c0c488bebf7bb042751f94243fda09a1
Jacob Kerr <jck268@cornell.edu>
2022-10-17 04:39:28 +0000

    Added all endpoints

--- userinfo/__init__.py
+++ userinfo/__init__.py
@@ -1,10 +1,12 @@
 """The user information microservices. Manages all user information stored in
 the database
+
 Actions
 -------
 get - get an existing user's information
 update - update an existing user's information
 add - adds a new user
+
 User Data Object
 ----------------
 id: int - id of the user
@@ -16,28 +18,79 @@ info: dict[str,str] - profile information
     time-zone: str - timezone of the user
     profile-picture-id: str - id of profile picture"""
 
+from typing import Mapping
+import os
+import json
 import logging
 
 import azure.functions as func
 from azure.cosmos.aio import CosmosClient
 from azure.cosmos.exceptions import CosmosHttpResponseError
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+ENDPOINT = os.environ.get("ENDPOINT") or ""
+KEY = os.environ.get("KEY") or ""
+DATABASE_NAME = os.environ.get("DATABASE_NAME") or ""
+CONTAINER_NAME = os.environ.get("CONTAINER_NAME") or ""
+PARTITION_KEY = os.environ.get("PARTITION_KEY") or ""
 
-    name = req.params.get('name')
-    if not name:
+client = CosmosClient(ENDPOINT, credential=KEY)
+database = client.get_database_client(DATABASE_NAME)
+container = database.get_container_client(CONTAINER_NAME)
+
+async def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('userinfo lambda triggered')
+
+    req_type = req.params.get('type')
+
+    if req_type == 'get':
+        username = req.params.get('username')
+        if username is None:
+            return func.HttpResponse('Request malformed: username missing', status_code=400)
         try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
+            user_object = await container.read_item(item=username,partition_key=PARTITION_KEY)
+        except CosmosHttpResponseError:
+            return func.HttpResponse('User does not exist', status_code=400)
         else:
-            name = req_body.get('name')
+            return func.HttpResponse(json.dumps(user_object), status_code=200)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    elif req_type == 'update':
+        username = req.params.get('username')
+        if username is None:
+            return func.HttpResponse('Request malformed: username missing', status_code=400)
+        user_object = await container.read_item(item=username,partition_key=PARTITION_KEY)
+        try:
+            body: dict[str,str] = req.get_json()
+            for key in body.keys():
+                user_object[key] = body[key]
+            container.replace_item(item=username,body=user_object)
+        except ValueError:
+            return func.HttpResponse('Request malformed: body not json', status_code=400)
+        except CosmosHttpResponseError:
+            return func.HttpResponse('User does not exist', status_code=400)
+        else:
+            return func.HttpResponse('Okay', status_code=200)
+
+    elif req_type == 'add':
+        try:
+            body: dict[str,str] = req.get_json()
+            container.create_item(body)
+        except ValueError:
+            return func.HttpResponse('Request malformed: body not json', status_code=400)
+        except CosmosHttpResponseError:
+            return func.HttpResponse('User already exists', status_code=400)
+        else:
+            return func.HttpResponse('Okay', status_code=200)
+        
+    elif req_type == 'delete':
+        username = req.params.get('username')
+        if username is None:
+            return func.HttpResponse('Request malformed: username missing', status_code=400)
+        try:
+            container.delete_item(username, partition_key=PARTITION_KEY)
+        except CosmosHttpResponseError:
+            return func.HttpResponse('User does not exist', status_code=400)
+        else:
+            return func.HttpResponse('Okay', status_code=200)
+    
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        return func.HttpResponse('Request malformed: unknown request type', status_code=400)
