dd74f3630603fc8206af775573aa894e890ccd6a
Jacob Kerr <jck268@cornell.edu>
2022-11-26 15:53:26 -0500

    Changed testfunc microservice to read given database

--- testfunc/__init__.py
+++ testfunc/__init__.py
@@ -1,24 +1,31 @@
+"""Test microservice, used for testing only. Not for production"""
+
+import os
+import json
 import logging
 
 import azure.functions as func
+from azure.cosmos.aio import CosmosClient
+from azure.cosmos.exceptions import CosmosHttpResponseError
+
+from dotenv import load_dotenv
+load_dotenv()
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+ENDPOINT = os.environ.get("ENDPOINT") or ""
+KEY = os.environ.get("KEY") or ""
+DATABASE_NAME = os.environ.get("DATABASE_NAME") or ""
+CONTAINER_NAME = os.environ.get("CONTAINER_NAME") or ""
+PARTITION_KEY = os.environ.get("PARTITION_KEY") or ""
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+client = CosmosClient(ENDPOINT, credential=KEY)
+database = client.get_database_client(DATABASE_NAME)
+container = database.get_container_client(CONTAINER_NAME)
+
+
+async def main(req: func.HttpRequest) -> func.HttpResponse:
+    result = ''
+    async for dict in container.read_all_items():
+        result += str(dict) + "\n"
+    return func.HttpResponse(result, status_code=200)
