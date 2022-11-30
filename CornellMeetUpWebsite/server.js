50ce290400defd993599024a70d96f08c9be300f
Mitchell Gray <meg346@cornell.edu>
2022-11-30 16:53:35 -0500

    Multiple user logins possible. Need to fix cor issue

--- CornellMeetUpWebsite/server.js
+++ CornellMeetUpWebsite/server.js
@@ -1,46 +1,134 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
+const ejs = require("ejs");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.use('*/css',express.static(__dirname + '/css'));
-app.use('*/js',express.static(__dirname + '/js'));
-app.use('*/images',express.static(__dirname + '/images'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use('*/css', express.static(__dirname + '/css'));
+app.use('*/js', express.static(__dirname + '/js'));
+app.use('*/images', express.static(__dirname + '/images'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname);
+
 
 app.get("/", function(req, res) {
-    res.redirect("/sign-in");
+  res.redirect("/sign-in");
 })
 
-app.post("/register", function(req, res) {
-    res.sendFile(__dirname + "/map.html");
-    const url = "https://api.kanye.rest";
+app.get("/register", function(req, res) {
+  res.sendFile(__dirname + "/register.html");
 
-    https.get(url, function(response)
-    {
-      console.log(response.statusCode);
-      response.on("data", function(data){
-        const output = JSON.parse(data);
-        console.log(output);
-      })
-    })
-    console.log(__dirname);
 })
 
 app.get("/sign-in", function(req, res) {
 
-      res.sendFile(__dirname + "/index.html");
-      // const url = "https://api.kanye.rest";
-      //
-      // https.get(url, function(response)
-      // {
-      //   console.log(response.statusCode);
-      //   response.on("data", function(data){
-      //     const output = JSON.parse(data);
-      //     console.log(output);
-      //   })
-      // })
-      // console.log(__dirname);
+  res.sendFile(__dirname + "/index.html");
+  // const url = "https://api.kanye.rest";
+  //
+  // https.get(url, function(response)
+  // {
+  //   console.log(response.statusCode);
+  //   response.on("data", function(data){
+  //     const output = JSON.parse(data);
+  //     console.log(output);
+  //   })
+  // })
+  // console.log(__dirname);
+})
+
+app.post("/map", function(req, res) {
+  var username = req.body.username;
+  res.render(__dirname + "/map.html", {
+    username: username
+  });
+})
+
+app.get("/map", function(req, res) {
+  var username = req.body.username;
+  res.render(__dirname + "/map.html", {
+    username: username
+  });
+})
+
+const options = {
+  method: 'POST'
+}
+
+
+// var postData = JSON.stringify({
+//   "id": "bruh_bob_jr",
+//   "last-online": "11/30/22",
+//   "groups": [1, 2],
+//   "info": {
+//     "birthday": "11/01/20",
+//     "time-zone": "EST",
+//     "profile-picture-id": "https://img.bleacherreport.net/img/images/photos/002/169/316/hillbillyjim0_crop_exact.jpg?w=423&h=281&q=75"
+//   }
+// });
+app.post("/create-user", function(req, res) {
+  // console.log(req.body);
+  var url = "https://cornellmeetup.azurewebsites.net/api/userinfo?type=add";
+  var postData = JSON.stringify({
+    "id": "users_"+req.body.username,
+    "last-online": "11/30/22",
+    "groups": [1],
+    "info": {
+      "birthday": req.body.birthday,
+      "time-zone": req.body.timezone,
+      "profile_picture_id": req.body.pfp
+    }
+  });
+  console.log("postData: " + postData);
+  var request = https.request(url, options, function(response) {
+    res.on('data', d => {
+      process.stdout.write(d);
+    })
+    console.log(response);
+  })
+  request.on('error', (e) => {
+    console.error(e);
+  });
+  request.write(postData);
+  request.end();
+
+  var username = req.body.username;
+  res.render(__dirname + "/map.html", {
+    username: username
+  });
+
+  var locationPostData = JSON.stringify({
+    "longitude": 0,
+    "latitude": 0
+  });
+
+  var url = "https://cornellmeetup.azurewebsites.net/api/locationinfo?type=add&username=" + req.body.username;
+  var request = https.request(url, options, function(response) {
+    res.on('data', d => {
+      process.stdout.write(d);
+    })
+    console.log(response);
+  })
+  request.on('error', (e) => {
+    console.error(e);
+  });
+  request.write(locationPostData);
+  request.end();
+  // res.sendFile(__dirname + "/map.html", {username:username});
+  // const url = "https://api.kanye.rest";
+  //
+  // https.get(url, function(response)
+  // {
+  //   console.log(response.statusCode);
+  //   response.on("data", function(data){
+  //     const output = JSON.parse(data);
+  //     console.log(output);
+  //   })
+  // })
+  // console.log(__dirname);
 })
 //
 // app.post("/", function(req, res) {
@@ -58,7 +146,6 @@ app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
 
-function getUserLocation(username)
-{
+function getUserLocation(username) {
   console.log("called method using: " + String(username));
 }
